6243b8a37b8c5a082846f309db58729dcad5160e
owfdr <owfdr@ourworld.center>
2023-04-26 19:27:19 +0900

    feat: check version

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import pkg from "../package.json";
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
   console.log("Hello World!");
+} else if (args[0] === "--version") {
+  console.log(pkg.version);
 } else {
   console.log(`Hello ${args[0]}!`);
 }
